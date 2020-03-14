a85278a7733ed78eb4f968c0b7cee1d1ea3551eb
crs2here <crs2here@gmail.com>
2020-03-14 13:28:48 -0700

    setup view

--- App.js
+++ App.js
@@ -1,38 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Fragment} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  TouchableOpacity
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Button = ({action, text}) => {
+  return (
+    <TouchableOpacity onPress={action} style={styles.button}>
+      <Text style={[styles.title, styles.buttonText]}>{text}</Text>
+    </TouchableOpacity>    
+  )
+}
 
 const App = () => {
+
+  const [timer, setTimer] = useState(false)
+  const [seconds, setSeconds] = useState(0)
+
+  useEffect(()=> {
+    let clock = (timer) ? setInterval(() => setSeconds(seconds+1),1000) : null;
+    return () => clearInterval(clock);
+  },[timer])
+
   return (
-    <Fragment>
-      <View style={styles.engine}>
-        <Text>hello worlds</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Track the time</Text>
+      <View style={styles.buttonContainer}>
+        <Button text={'Start'} action={()=> setTimer(true)} />
+        <Button text={'Stop'} action={()=> setTimer(false)}/>
+        <Button text={'Clear'} action={()=> setSeconds(0)}/>
       </View>
-    </Fragment>
+      <Text style={styles.title}>{seconds}</Text>
+      
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 28,
+    color: '#fff',
+    textAlign: 'center',
+    marginBottom: 5,    
+  },
+  button: {
+    backgroundColor: 'lightblue',
+    borderRadius: 5,
+    borderWidth: 1,
+    width: 80,
+    marginBottom: 10,
+    borderColor: 'white'
+  },
+  buttonText: {
+    color: 'black',
+  },
+  buttonContainer: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    alignContent: 'center',
+    justifyContent: 'space-around',
+    flexDirection: 'row'
+  }  
+});
 
 
 export default App;
