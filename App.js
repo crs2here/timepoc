72b637c649ac9e754c8bb8f5501a10900492aa28
crs2here <crs2here@gmail.com>
2020-03-14 08:14:42 -0700

    init commit

---
+++ App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <Fragment>
+      <View style={styles.engine}>
+        <Text>hello worlds</Text>
+      </View>
+    </Fragment>
+  );
+};
+
+
+export default App;
